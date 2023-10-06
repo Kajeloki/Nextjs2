795f2bba00e222bfe4edb5c0926184a08c8174e0
Kajeloki <piotrkw200@wp.pl>
2023-10-06 19:14:35 +0200

    podstawy nextjs

---
+++ pages/news/[newsId].js
@@ -0,0 +1,9 @@
+import {useRouter} from 'next/router';
+
+const DetailPage = () =>{
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+    return <h1>Witaj na stronie ze szczegółami!</h1>
+}
+export default DetailPage;
---
+++ pages/news/index.js
@@ -0,0 +1,6 @@
+//our-domain.cmo/news
+
+const NewsPage = () =>{
+    return <h1>Witaj na stronie z wiadomościami!</h1>
+}
+export default NewsPage;    
