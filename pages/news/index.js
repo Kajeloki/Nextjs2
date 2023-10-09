347496410502218fedb9bd78573c5957b12bcea9
Kajeloki <piotrkw200@wp.pl>
2023-10-09 11:09:32 +0200

    koniec

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,6 +1,13 @@
 //our-domain.cmo/news
+import Link from 'next/link'
 
 const NewsPage = () =>{
-    return <h1>Witaj na stronie z wiadomościami!</h1>
+    return <>
+    <h1>Witaj na stronie z wiadomościami!</h1>
+    <ul>
+        <li><Link href="/news/NextJS-to-świetny-Framework">NextJS to świetny Framework</Link></li>
+        <li>Jakiś tak artykuł</li>
+    </ul>
+    </> 
 }
 export default NewsPage;    
